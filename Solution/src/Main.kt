2c68f8d5ae1bf5763101b326d076863f6b3f09dd
Nedim Toric <nedimtoric@icloud.com>
2024-10-26 15:30:00 +0200

    User can input path to the file with instructions

--- Solution/src/Main.kt
+++ Solution/src/Main.kt
@@ -1,14 +1,16 @@
 import java.io.File
 import java.io.InputStream
 
-fun procitajFile(path: String) : List<String>{
-    val inputStream : InputStream = File(path).inputStream()
+fun procitajFile() : List<String>{
+    print("Unesi putanju do fajla sa instrukcijama: ")
+    val userPath = readln()
+    val inputStream : InputStream = File(userPath).inputStream()
     val lineList = mutableListOf<String>()
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
     return lineList
 }
 
 fun main() {
-    val instrukcije : List<String> = procitajFile("Solution/src/instrukcije.txt")
+    val instrukcije : List<String> = procitajFile()
     println(Dekoder(instrukcije, Tastatura()).dekodiraj())
 }
