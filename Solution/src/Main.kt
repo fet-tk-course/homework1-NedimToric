7510c0f612f540c88782f495557b02a3404ce04a
Nedim Toric <nedimtoric@icloud.com>
2024-10-26 15:07:13 +0200

    Adding reading from file capabilites

--- Solution/src/Main.kt
+++ Solution/src/Main.kt
@@ -1,9 +1,14 @@
+import java.io.File
+import java.io.InputStream
+
+fun procitajFile(path: String) : List<String>{
+    val inputStream : InputStream = File(path).inputStream()
+    val lineList = mutableListOf<String>()
+    inputStream.bufferedReader().forEachLine { lineList.add(it) }
+    return lineList
+}
+
 fun main() {
-    val listaInstrukcija = listOf(
-        "^<<",
-        ">>vvv",
-        "<^>v<",
-        "^^^^v"
-    )
-    println(Dekoder(listaInstrukcija, Tastatura()).dekodiraj())
+    val instrukcije : List<String> = procitajFile("Solution/src/instrukcije.txt")
+    println(Dekoder(instrukcije, Tastatura()).dekodiraj())
 }
--- Solution/src/Tastatura.kt
+++ Solution/src/Tastatura.kt
@@ -1,12 +1,9 @@
-class Tastatura : Instrukcija{
+class Tastatura {
     var tastatura = arrayOf(
         arrayOf(1, 2, 3),
         arrayOf(4, 5, 6),
         arrayOf(7, 8, 9)
     )
-    override fun pomjeri(pozicija: Int) : Int {
-        return 1
-    }
     fun pomjeri(instrukcija: Char, pozicija: Int) : Int{
         val novaPozicija = when(instrukcija) {
             '^' -> Gore().pomjeri(pozicija)
